2eaf284c3a910db6e6095fc78615c6c079448140
Ian Perez Bunuel <C00294680@setu.ie>
2026-09-14 11:13:22 +0100

    Added 1 failed test

--- Sprint1/src/lib/filters/collatz.test.ts
+++ Sprint1/src/lib/filters/collatz.test.ts
@@ -4,7 +4,7 @@ import { collatzMulti } from './collatz';
 describe('Cube filter', () => {
   it('collatz a sequence', () => {
     const input = [2, 5, 21, 52, 999, 0]; // 6
-    const expected = [1, 16, 64, 26, 2998, 0];
+    const expected = [1, 16, 64, 26, 298, 0];
     expect(collatzMulti(input)).toEqual(expected);
   });
 });
